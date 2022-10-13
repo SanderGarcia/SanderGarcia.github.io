94a3641eb7217223b1c0fb11b3e63190ce399495
SanderGarcia <sanderlfg@gmail.com>
2022-10-13 12:51:45 -0300

    Inclusão de idade dinâmica

--- script.js
+++ script.js
@@ -37,3 +37,24 @@ function tamanhoPagina() {
   }
 }
 tamanhoPagina();
+
+function idade(ano_aniversario, mes_aniversario, dia_aniversario) {
+  const campo = document.getElementById('data');
+  var d = new Date,
+    ano_atual = d.getFullYear(),
+    mes_atual = d.getMonth() + 1,
+    dia_atual = d.getDate(),
+
+    ano_aniversario = +ano_aniversario,
+    mes_aniversario = +mes_aniversario,
+    dia_aniversario = +dia_aniversario,
+
+    quantos_anos = ano_atual - ano_aniversario;
+
+    if (mes_atual < mes_aniversario || mes_atual ==   mes_aniversario && dia_atual < dia_aniversario) {
+        quantos_anos--;
+    }
+  const idade = quantos_anos < 0 ? 0 : quantos_anos;
+  campo.innerText = idade;
+}
+idade(1986, 10, 30);
